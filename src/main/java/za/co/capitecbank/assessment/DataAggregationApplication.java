8fa1ca3f1d952b22f32a3636ce07ecd076c6ab7f
Liza Madubela <LizaMadubela+capitec@capitecbank.co.za>
2025-11-24 16:19:29 +0200

    Initial commit

---
+++ src/main/java/za/co/capitecbank/assessment/DataAggregationApplication.java
@@ -0,0 +1,13 @@
+package za.co.capitecbank.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAggregationApplication.class, args);
+    }
+
+}
