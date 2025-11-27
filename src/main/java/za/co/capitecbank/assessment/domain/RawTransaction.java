703e0c428a514e3a0abcd23fa92891aea09ac82c
Liza Madubela <LizaMadubela+capitec@capitecbank.co.za>
2025-11-27 08:28:22 +0200

    Domain tx object.

--- src/main/java/za/co/capitecbank/assessment/domain/RawTransaction.java
+++ src/main/java/za/co/capitecbank/assessment/domain/RawTransaction.java
@@ -1,4 +1,26 @@
 package za.co.capitecbank.assessment.domain;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
 public class RawTransaction {
+    private final String accountId;
+    private final String description;
+    private final BigDecimal amount;
+    private final LocalDateTime timestamp;
+
+
+    public RawTransaction(String accountId, String description, BigDecimal amount, LocalDateTime timestamp) {
+        this.accountId = accountId;
+        this.description = description;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+
+    public String getAccountId() { return accountId; }
+    public String getDescription() { return description; }
+    public BigDecimal getAmount() { return amount; }
+    public LocalDateTime getTimestamp() { return timestamp; }
 }
