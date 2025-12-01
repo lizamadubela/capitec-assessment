702ddca1c6c536c68171a46d341ff0696f225d17
Liza Madubela <LizaMadubela+capitec@capitecbank.co.za>
2025-12-01 20:39:26 +0200

    added unit tests.

--- src/test/java/za/co/capitecbank/assessment/DataAggregationApplicationTests.java
+++ src/test/java/za/co/capitecbank/assessment/DataAggregationApplicationTests.java
@@ -1,13 +1,13 @@
-//package za.co.capitecbank.assessment;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DataAggregationApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}/.
+package za.co.capitecbank.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAggregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
