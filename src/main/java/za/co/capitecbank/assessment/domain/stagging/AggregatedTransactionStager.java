c215416a668ca4e0c96e5fbe49cd7b1355972ee5
Liza Madubela <LizaMadubela+capitec@capitecbank.co.za>
2025-12-05 15:15:40 +0200

    Refactoring ...

--- src/main/java/za/co/capitecbank/assessment/domain/stagging/AggregatedTransactionStager.java
+++ src/main/java/za/co/capitecbank/assessment/domain/stagging/AggregatedTransactionStager.java
@@ -6,7 +6,7 @@ import za.co.capitecbank.assessment.domain.entity.AggregatedTransaction;
 import za.co.capitecbank.assessment.domain.entity.RawTransaction;
 import za.co.capitecbank.assessment.repository.AggregatedTransactionRepository;
 import za.co.capitecbank.assessment.service.TxCategorizationEngine;
-import za.co.capitecbank.assessment.tx_source.TransactionSource;
+import za.co.capitecbank.assessment.transactions.source.TransactionSource;
 
 import java.util.List;
 import java.util.stream.Collectors;
